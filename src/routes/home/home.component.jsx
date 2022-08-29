5edc0f06062b3ef5fd379fff10f09866db34bbd4
Andrei <91586968+AndreiRutty@users.noreply.github.com>
2022-08-29 10:14:52 +0400

    Add files via upload

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Directory from "../../components/directory/directory.component";
+import { categories } from "../../categories/categories";
+
+const Home = () => {
+  return (
+    <div>
+      <Outlet />
+      <Directory categories={categories} />
+    </div>
+  );
+};
+
+export default Home;
