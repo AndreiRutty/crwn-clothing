e770860ea7e66ab8c2e572bfd662e944b6aaba7c
Andrei Rutty <andreirutty.ar20@gmail.com>
2022-09-05 09:22:56 +0400

    Last Touch before initial deploy

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -3,13 +3,17 @@ import {
   Body,
   DirectoryItemContainer,
 } from "./directory-item.styles.jsx";
+import { useNavigate } from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
-      <Body to={`shop/${title}`}>
+      <Body>
         <h2>{title.toUpperCase()}</h2>
         <p>Shop Now</p>
       </Body>
